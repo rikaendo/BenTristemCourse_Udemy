d4a2298269a865f46aa5f6ed9a7e6ce66e74bdb0
Rika Nishikawa <rikaendo@alum.calarts.edu>
2018-05-17 00:24:20 -0700

    lecture 135 done, linetraced from camera's crosshair to landscape hit location

--- Section_04/BattleTank/Source/BattleTank/CPP_PlayerController_Tank.cpp
+++ Section_04/BattleTank/Source/BattleTank/CPP_PlayerController_Tank.cpp
@@ -3,6 +3,7 @@
 #include "CPP_PlayerController_Tank.h"
 #include "CPP_Tank.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 
 void ACPP_PlayerController_Tank::BeginPlay()
@@ -37,28 +38,63 @@ void ACPP_PlayerController_Tank::AimTowardsCrossHair()
 {
 	if (!GetControlledTank()) { return; }
 
-	FVector OutHitLocation; //Out parameter
-	if (GetSightRayHitLocation(OutHitLocation))//Has "side-effect", is going to line trace
+	FVector HitLocation; //Out parameter
+	if (GetSightRayHitLocation(HitLocation))//Has "side-effect", is going to line trace
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *OutHitLocation.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString());
 		//TODO tell controlled tank to aim at this point
 	}
 }
 
 //Get world location of linetrace through crosshair, true if it hits landscape
-bool ACPP_PlayerController_Tank::GetSightRayHitLocation(FVector& OutHitLocation) const
+bool ACPP_PlayerController_Tank::GetSightRayHitLocation(FVector& HitLocation) const
 {
 	//Find crosshair position in pixel coordinates
 	int32 ViewportSizeX, ViewportSizeY;
 	GetViewportSize(ViewportSizeX, ViewportSizeY);
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
-
-
-	//deproject screen position to world direction
-	//linetrace along that direction
 	
-	/* FVector LineTraceStart = AimPoint from PlayerUI
-	FVector LineTraceEnd = LineTraceStart + Rotation * Length
-	GetWorld()->LineTraceSingleByObjectType(){}*/
+	//deproject screen position to world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		//linetrace along that direction
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
 	return true;
 }
+
+bool ACPP_PlayerController_Tank::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation; //to be discarded
+	return DeprojectScreenPositionToWorld
+	(
+		ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection
+	);
+}
+
+bool ACPP_PlayerController_Tank::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+
+	if (GetWorld()->LineTraceSingleByChannel
+			(
+			HitResult,
+			StartLocation,
+			EndLocation,
+			ECollisionChannel::ECC_Visibility
+			)
+		)
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	HitLocation = FVector(0);
+	return false;
+}
+
--- Section_04/BattleTank/Source/BattleTank/CPP_PlayerController_Tank.h
+++ Section_04/BattleTank/Source/BattleTank/CPP_PlayerController_Tank.h
@@ -29,11 +29,18 @@ private:
 	void AimTowardsCrossHair();
 
 	//returns OUT parameter, true if hits landscape
-	bool GetSightRayHitLocation(FVector& OutHitLocation) const;	
+	bool GetSightRayHitLocation(FVector& HitLocation) const;	
 	
 	UPROPERTY(EditAnywhere)
 	float CrosshairXLocation = 0.5;
 
 	UPROPERTY(EditAnywhere)
 	float CrosshairYLocation = 0.33333;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000.0f;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
